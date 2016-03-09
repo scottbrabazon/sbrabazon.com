45ad4cb6b4547aa86d1742e3444ead05922c8cad
Scott Brabazon <scottbrabazon@Scotts-Mac-mini.home>
2016-03-09 21:54:30 +0000

    Improvements

--- dev/wordpress/wp-content/themes/jesters/functions.php
+++ dev/wordpress/wp-content/themes/jesters/functions.php
@@ -10,4 +10,13 @@ require_once 'engine/bootstrap.php';
 	Add your code below.
 */
 
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
+
 ?>
